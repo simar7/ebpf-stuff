ebf1c4078c3191bb566a09f2541a7d4acc4c3227
Simarpreet Singh <simar@linux.com>
2019-10-21 14:28:27 -0700

    helloworld: add a simple cmdRun()
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+func cmdRun(args []string) {
+	cmd := exec.Command(args[0], args[1:]...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Print("cmd.Run() returned an error: ", err)
+	}
+	fmt.Println("combined output: ", string(out))
+}
 
 func main() {
 	fmt.Println("Hello, eBPF World!")
+	cmdRun([]string{"ls", "-lrtah"})
 }
