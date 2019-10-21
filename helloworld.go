19e413eb33ff46e6a1c5ef56504095a4f1f7f597
Simarpreet Singh <simar@linux.com>
2019-10-21 14:45:13 -0700

    helloworld: Add writeFile and readFile funcs
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- helloworld.go
+++ helloworld.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
 )
 
@@ -10,12 +12,41 @@ func cmdRun(args []string) {
 	cmd := exec.Command(args[0], args[1:]...)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Print("cmd.Run() returned an error: ", err)
+		log.Println("cmd.Run() returned an error: ", err)
 	}
 	fmt.Println("combined output: ", string(out))
+}
+
+func writeFile(dir string) *os.File {
+	f, err := ioutil.TempFile(dir, "helloworld-*.tmp")
+	if err != nil {
+		log.Println("unable to create temp file: ", err)
+		return nil
+	}
+	_, err = f.Write([]byte(`A quick brown fox jumps over the lazy little dog.`))
+	if err != nil {
+		log.Println("unable to write to temp file: ", err)
+		return nil
+	}
+
+	return f
+}
+
+func readFile(fileName string) {
+	b, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Println("unable to read file: ", err)
+	}
+	log.Println("read file contents: ", string(b))
 }
 
 func main() {
 	fmt.Println("Hello, eBPF World!")
 	cmdRun([]string{"ls", "-lrtah"})
+	f := writeFile("/")
+	defer f.Close()
+
+	if f != nil {
+		readFile(f.Name())
+	}
 }
